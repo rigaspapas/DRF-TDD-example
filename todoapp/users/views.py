88a0afa5d0550fc1fd3de15960ffdb14aa4eb393
Rigas Papas <rigaspapas@gmail.com>
2018-09-16 22:59:58 +0300

    Extend the token-related endpoints
    Implement the /tokens/<pk>/ endpoint that supports GET and DELETE HTTP
    methods. Use the RetrieveDestroyAPIView in order to better showcase
    Django REST framework's capabilities.
    
    Remove the logout endpoint.
    
    Add the 'created' field in Token's serializer.

--- todoapp/users/views.py
+++ todoapp/users/views.py
@@ -2,7 +2,7 @@ from rest_framework import status
 from rest_framework.authtoken.models import Token
 from rest_framework.generics import CreateAPIView, GenericAPIView
 from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import RetrieveDestroyAPIView
 from users.serializers import UserRegistrationSerializer, UserLoginSerializer, TokenSerializer
 
 
@@ -46,8 +46,22 @@ class UserLoginAPIView(GenericAPIView):
             )
 
 
-class UserLogoutAPIView(APIView):
+class UserTokenAPIView(RetrieveDestroyAPIView):
+    serializer_class = TokenSerializer
+    queryset = Token.objects.all()
 
-    def post(self, request, *args, **kwargs):
-        Token.objects.filter(user=request.user).delete()
-        return Response(status=status.HTTP_200_OK)
+    def filter_queryset(self, queryset):
+        return queryset.filter(user=self.request.user)
+
+    def retrieve(self, request, pk, *args, **kwargs):
+        if pk == "current":
+            instance = Token.objects.get(key=request.auth.key)
+            serializer = self.get_serializer(instance)
+            return Response(serializer.data)
+        return super(UserTokenAPIView, self).retrieve(request, pk, *args, **kwargs)
+
+    def destroy(self, request, pk, *args, **kwargs):
+        if pk == "current":
+            Token.objects.get(key=request.auth.key).delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        return super(UserTokenAPIView, self).destroy(request, pk, *args, **kwargs)
